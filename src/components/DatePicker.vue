8208105b4c2ae12272988dde013ab25c50c299c6
Evgen904 <evgen904@gmail.com>
2018-11-26 13:47:45 +0400

    added class btn-show-datepicker to open calendar

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -377,8 +377,11 @@
         this.$parent.$emit('inputChanged', this.inputValue);
       },
       handleOutsideClick() {
-        if (this.isOpen) {
+        if (this.isOpen || event.target.classList.contains('btn-show-datepicker') == false) {
           this.hideDatepicker();
+        }
+        else {
+          this.isOpen = true;
         }
       },
       hideDatepicker() {
@@ -937,14 +940,14 @@
             }
 
             &:first-child {
-                background: transparent url('mdash.svg') no-repeat right center / 8px;
+                background: transparent url('mdash.svg') no-repeat right center / 9px;
                 padding-right: 14px;
                 border-top-left-radius: 3px;
                 border-bottom-left-radius: 3px;
             }
 
             &:last-child {
-              background: transparent url('mdash.svg') no-repeat left center / 8px;
+              background: transparent url('mdash.svg') no-repeat left center / 9px;
               padding-left: 4px;
               border-top-right-radius: 3px;
               border-bottom-right-radius: 3px;
